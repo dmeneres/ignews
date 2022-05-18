fb956f936975e60360faf44f1aa2debe9d40c28f
Diogo Menéres <dmeneres@gmail.com>
2022-05-18 10:12:43 +0100

    chapter 3 finished

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,7 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { ActiveLink } from '../ActiveLink'
+
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
@@ -7,8 +11,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} href="#">Home</a>
-          <a href="#">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
